411f628382ec244854749aa45a125c5a43f566b6
anhnbt-it <anhnbt.it@gmail.com>
2020-10-29 10:50:43 +0700

    refactor: add final AccountValidateExampleTest

--- test/AccountValidateExampleTest.java
+++ test/AccountValidateExampleTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class AccountValidateExampleTest {
 
-    private AccountValidateExample accountValidate = new AccountValidateExample();
+    private final AccountValidateExample accountValidate = new AccountValidateExample();
 
     @Test
     @DisplayName("123abc")
